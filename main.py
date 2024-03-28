41e404a5ecbe417f82c8f36ec77490a24126371f
Your Name <you@example.com>
2024-03-28 10:59:43 +0200

    added main, .gitignore and .env.example

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print(os.getenv('TELEGRAM_TOKEN'))
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
