741e2ae3d9c49693c898c08775fb50f31bbf58d4
Your Name <you@example.com>
2024-03-28 13:59:01 +0200

    updated TELEGRAM_TOKEN_BOT

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import os
-
+import logging
 
 from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 load_dotenv()
 
-print(os.getenv('TELEGRAM_TOKEN'))
+TELEGRAM_TOKEN_BOT = os.getenv('TELEGRAM_TOKEN_BOT')
 
 
-def print_hi(name):
-    print(f'Hi, {name}')
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+app = ApplicationBuilder().token(TELEGRAM_TOKEN_BOT).build()
+
+app.add_handler(CommandHandler("hello", hello))
+
+app.run_polling()
