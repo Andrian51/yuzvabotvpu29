e53c077fe738ea4dd4692ac656459cf02f5f62c3
Your Name <you@example.com>
2024-03-28 15:43:11 +0200

    added автор та бувай

--- main.py
+++ main.py
@@ -22,24 +22,34 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Start {update.effective_user.first_name}')
 
 
+async def author(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Мене робив Diduh Andrian")
+
+
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    messege = update.message.text.lower()
+    message = update.message.text.lower()
 
-    if 'привіт' in messege:
-        reply_text = f'Привіт {update.effective_user.first_name} !'
-    else:
-        reply_text = 'Я тебе не розумію, чел'
+    last_name = update.effective_user.last_name
+    if "привіт" or "ку" in message:
+        if last_name:
+            await update.message.reply_text(
+                f"привіт {update.effective_user.first_name} {update.effective_user.last_name}")
+        else:
+            await update.message.reply_text(f"привіт {update.effective_user.first_name}")
 
-    if 'бувай' or 'прощавай' in messege:
-        reply_text = f'Прощавай {update.effective_user.first_name} !'
-
-    await update.message.reply_text(reply_text)
+    elif "прощавай" or "папа" in message:
+        if last_name:
+            await update.message.reply_text(
+                f"прощавай {update.effective_user.first_name} {update.effective_user.last_name}")
+        else:
+            await update.message.reply_text(f"прощавай {update.effective_user.first_name}")
 
 
 app = ApplicationBuilder().token(TELEGRAM_TOKEN_BOT).build()
 
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("author", author))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
 app.run_polling()
