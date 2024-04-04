4cf8068fba0f37d98533b7b407a1ff265f3fcdcb
Your Name <you@example.com>
2024-04-04 14:18:51 +0300

    updated TELEGRAM_TOKEN_BOT

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import logging
 
 from dotenv import load_dotenv
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 
 load_dotenv()
@@ -19,30 +19,23 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Start {update.effective_user.first_name}')
+    keyboard = [
+        [KeyboardButton('Hello'), KeyboardButton('Word')],
+        [KeyboardButton('Good buy')],
+    ]
+
+    reply_markup = ReplyKeyboardMarkup(keyboard)
+
+    await update.message.reply_text(
+        f'Привіт {update.effective_user.first_name}',
+        reply_markup=reply_markup)
 
 
 async def author(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Мене робив Diduh Andrian")
 
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    message = update.message.text.lower()
-
-    last_name = update.effective_user.last_name
-    if "привіт" or "ку" in message:
-        if last_name:
-            await update.message.reply_text(
-                f"привіт {update.effective_user.first_name} {update.effective_user.last_name}")
-        else:
-            await update.message.reply_text(f"привіт {update.effective_user.first_name}")
-
-    elif "прощавай" or "папа" in message:
-        if last_name:
-            await update.message.reply_text(
-                f"прощавай {update.effective_user.first_name} {update.effective_user.last_name}")
-        else:
-            await update.message.reply_text(f"прощавай {update.effective_user.first_name}")
+async def Bye(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="пращавай")
 
 
 app = ApplicationBuilder().token(TELEGRAM_TOKEN_BOT).build()
@@ -50,6 +43,6 @@ app = ApplicationBuilder().token(TELEGRAM_TOKEN_BOT).build()
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("author", author))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+app.add_handler(CommandHandler("Bye", Bye))
 
 app.run_polling()
